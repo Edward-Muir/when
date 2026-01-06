a6b7e39f6a7844c02e0d14d17cd5fc4f63e50c0a
Edward Muir <edwardcmuir@gmail.com>
2026-01-06 20:53:02 +0000

    fix: Remove duplicate events

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = 'when-v0.2.2';
-const STATIC_CACHE = 'when-static-v0.2.2';
-const DYNAMIC_CACHE = 'when-dynamic-v0.2.2';
+const CACHE_NAME = 'when-v0.3.0';
+const STATIC_CACHE = 'when-static-v0.3.0';
+const DYNAMIC_CACHE = 'when-dynamic-v0.3.0';
 
 // Static assets to cache on install
 const STATIC_ASSETS = [
