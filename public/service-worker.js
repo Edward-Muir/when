e0c5e138947d2690ed3f7e3e60d7e91e9b1aeeb2
Edward Muir <edwardcmuir@gmail.com>
2026-01-06 23:12:02 +0000

    fix: Update image size

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = 'when-v0.3.0';
-const STATIC_CACHE = 'when-static-v0.3.0';
-const DYNAMIC_CACHE = 'when-dynamic-v0.3.0';
+const CACHE_NAME = 'when-v0.3.1';
+const STATIC_CACHE = 'when-static-v0.3.1';
+const DYNAMIC_CACHE = 'when-dynamic-v0.3.1';
 
 // Static assets to cache on install
 const STATIC_ASSETS = [
