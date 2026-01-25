3e144448afada71b81e0d464c0bc6704047d8c21
Edward Muir <edwardcmuir@gmail.com>
2026-01-25 12:33:57 +0000

    chore(release): 0.3.13

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = 'when-v0.3.12';
-const STATIC_CACHE = 'when-static-v0.3.12';
-const DYNAMIC_CACHE = 'when-dynamic-v0.3.12';
+const CACHE_NAME = 'when-v0.3.13';
+const STATIC_CACHE = 'when-static-v0.3.13';
+const DYNAMIC_CACHE = 'when-dynamic-v0.3.13';
 
 // Static assets to cache on install
 const STATIC_ASSETS = [
